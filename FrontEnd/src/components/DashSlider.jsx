84e54158aa22d656288544202d8ea0751450c142
@THIYAGUSRI <thiyagusrinivasan09@gmail.com>
2024-09-28 13:44:49 +0530

    update the code

--- FrontEnd/src/components/DashSlider.jsx
+++ FrontEnd/src/components/DashSlider.jsx
@@ -1,10 +1,13 @@
 import { Sidebar } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function DashSlider() {
-    const location = useLocation()
-  const [ tab, setTab ] = useState('')
+    const location = useLocation();
+  const [ tab, setTab ] = useState('');
+  const dispatch = useDispatch();
   useEffect(() => {
     const urlparams = new URLSearchParams(location.search);
     const tabFromUrl = urlparams.get('tab');
@@ -13,6 +16,23 @@ export default function DashSlider() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+
+  const handleSignout = async () => {
+    try {
+        const res = await fetch('/api/user/signout', {
+          method: 'POST',
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+            dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+  };
+
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
@@ -32,6 +52,9 @@ export default function DashSlider() {
                     Profile
                 </Sidebar.Item>
                 </Link>
+                <Sidebar.Item onClick={handleSignout} className='cursor-pointer'>
+                   Sign Out
+                </Sidebar.Item>
             </Sidebar.ItemGroup>
         </Sidebar.Items>
     </Sidebar>
